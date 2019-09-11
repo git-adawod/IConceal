3eb511a730a8643167a0a59a907dd87f3b76d7f1
git_adawod <andrewdawod98@hotmail.com>
2019-09-11 14:27:44 +0100

    Text steganography implementation

---
+++ TextSteganographyLibrary/ZeroWidthConverter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+
+namespace TextSteganographyLibrary
+{
+    public class ZeroWidthConverter
+    {
+        public static string Embed(string payload, string secret)
+        {
+            string zeroWidthString = StringToZeroWidth(payload, secret);
+            return zeroWidthString + payload;
+        }
+
+        private static string StringToZeroWidth(string payload, string secret)
+        {
+            string[] distinctWords = secret.Trim().Split(' ');
+
+            StringBuilder zeroWidthString = new StringBuilder();
+
+            foreach(string s in distinctWords)
+            {
+                zeroWidthString.Append(WordToZeroWidth(s));
+                zeroWidthString.Append('\uFEFF');
+            }
+
+            return zeroWidthString.ToString();
+
+        }
+
+        private static string WordToZeroWidth(string word)
+        {
+            char[] distinctCharacters = word.ToCharArray();
+            StringBuilder zeroWidthWord = new StringBuilder();
+
+            foreach(char c in distinctCharacters)
+            {
+                zeroWidthWord.Append(CharacterToZeroWidth(c));
+                zeroWidthWord.Append('\u200D');
+            }
+
+            zeroWidthWord.Remove(zeroWidthWord.Length - 1, 1);
+
+            return zeroWidthWord.ToString();
+        }
+
+        private static string CharacterToZeroWidth(char c)
+        {
+            string characterInBinary = CharacterToBinary(c);
+
+            StringBuilder zeroWidthCharacter = new StringBuilder();
+            
+            foreach(char bit in characterInBinary)
+            {
+                if(bit == '1')
+                {
+                    zeroWidthCharacter.Append('\u200B');
+                }
+                else if(bit == '0')
+                {
+                    zeroWidthCharacter.Append('\u200C');
+                }
+            }
+
+            return zeroWidthCharacter.ToString();
+        }
+
+        private static string CharacterToBinary(char c)
+        {
+            return Convert.ToString(c, 2).PadLeft(8, '0');
+        }
+    }
+}
---
+++ TextSteganographyLibrary/ZeroWidthExtractor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSteganographyLibrary
+{
+    public class ZeroWidthExtractor
+    {
+        public static string Extract(string steganoText)
+        {
+            StringBuilder zeroWidthString = new StringBuilder();
+
+            for(int i = 0; i < steganoText.Length; i++)
+            {
+                switch (steganoText[i])
+                {
+                    case '\u200B':
+                        zeroWidthString.Append(steganoText[i]);
+                        break;
+                    case '\u200C':
+                        zeroWidthString.Append(steganoText[i]);
+                        break;
+                    case '\u200D':
+                        zeroWidthString.Append(steganoText[i]);
+                        break;
+                    case '\uFEFF':
+                        zeroWidthString.Append(steganoText[i]);
+                        break;
+                }
+            }
+
+            return ZeroWidthToString(zeroWidthString.ToString());
+        }
+
+        private static string ZeroWidthToString(string zeroWidthString)
+        {
+            StringBuilder decipheredWords = new StringBuilder();
+            string[] distinctWords = zeroWidthString.Split('\uFEFF');
+
+
+            for(int i = 0; i < distinctWords.Length - 1; i++)
+            {
+                decipheredWords.Append(ZeroWidthToWord(distinctWords[i]));
+            }
+
+            decipheredWords.Remove(decipheredWords.Length - 1, 1);
+
+            return decipheredWords.ToString();
+        }
+
+        private static string ZeroWidthToWord(string word)
+        {
+            string[] distinctCharacters = word.Split('\u200D');
+            StringBuilder decipheredWord = new StringBuilder();
+
+            for(int i = 0; i < distinctCharacters.Length; i++)
+            {
+                decipheredWord.Append(ZeroWidthToCharacter(distinctCharacters[i]));
+            }
+
+            decipheredWord.Append(' ');
+
+            return decipheredWord.ToString();
+        }
+
+        private static char ZeroWidthToCharacter(string zeroWidthLetter)
+        {
+            StringBuilder letterInBinary = new StringBuilder();
+
+            for(int i = 0; i < zeroWidthLetter.Length; i++)
+            {
+                if (zeroWidthLetter[i] == '\u200B')
+                {
+                    letterInBinary.Append('1');
+                }
+                else if (zeroWidthLetter[i] == '\u200C')
+                {
+                    letterInBinary.Append('0');
+                }
+            }
+
+            return BinaryToCharacter(letterInBinary.ToString());
+        }
+
+        private static char BinaryToCharacter(string characterInBinary)
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.Add(Convert.ToByte(characterInBinary.Substring(0, 8), 2));
+            return Encoding.ASCII.GetString(bytes.ToArray())[0];
+        }
+    }
+}
