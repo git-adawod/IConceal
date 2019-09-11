8b248060bc52b78248cc9c8aa9668442a8a170bb
git_adawod <andrewdawod98@hotmail.com>
2019-09-11 21:27:19 +0100

    Added BinaryEmbed Struct for simplicity

---
+++ ImageSteganographyLibrary/BinaryEmbedFiles/BinaryEmbedStruct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSteganographyLibrary
+{
+    public struct BinaryEmbedStruct
+    {
+        public FileInfo Image;
+        public List<FileInfo> Files;
+        public DirectoryInfo Directory;
+        public FileInfo ZipFileLocation;
+    }
+}
